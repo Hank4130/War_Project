7453bd53eb33aad0031dc1b42cb40bc4ac9e059a
Hank4130 <hhd3600@gmail.com>
2016-03-04 09:54:14 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,44 @@
-//memes
-public static void help(int x){
-  System.out.println("help");
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class board extends JFrame implements ActionListener{
+
+	
+	private JButton[] theButtons = new JButton[12];
+	private int turn = 0;
+	
+			
+	public board(){
+		this.setSize(200, 200);
+		this.setVisible(true);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setLayout(new GridLayout(3, 4));
+		buildBoard();
+		
+		
+	}
+			
+	public void buildBoard() {
+
+		for (int i = 0; i < theButtons.length; i++) {
+			theButtons[i] = new JButton("");
+			theButtons[i].setBackground(Color.WHITE);
+			this.getContentPane().add(theButtons[i]);
+			theButtons[i].addActionListener(this);
+
+		}
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+		
+	}
+	
 }
