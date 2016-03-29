90351223393a2ce0962ae0e1fb2f02bdf5d5ac5e
Hank4130 <hhd3600@gmail.com>
2016-03-29 09:55:20 -0400

    Update board.java

--- board.java
+++ board.java
@@ -9,13 +9,15 @@ import javax.swing.JFrame;
 
 public class board extends JFrame implements ActionListener{
 	
-	private ImageIcon[] picts = { new ImageIcon("ace.png"),
-			new ImageIcon("2.png"),new ImageIcon("3.png"),
-			new ImageIcon("4.png"),new ImageIcon("5.png"),
-			new ImageIcon("6.png"),new ImageIcon("7.png"),
-			new ImageIcon("8.png"),new ImageIcon("jack.png"),
-			new ImageIcon("queen.png"),new ImageIcon("king.png"),
-			new ImageIcon("9.png"),new ImageIcon("10.png"), new ImageIcon("blueDeck.png"), new ImageIcon("BrownDeck.png")};
+	private ImageIcon[] picts = {new ImageIcon("2.png") 
+			,new ImageIcon("3.png"),new ImageIcon("4.png"),
+			new ImageIcon("5.png"),new ImageIcon("6.png"),
+			new ImageIcon("7.png"),new ImageIcon("8.png"),
+			new ImageIcon("9.png"),new ImageIcon("10.png"),
+			new ImageIcon("jack.png"),new ImageIcon("queen.png"),
+			new ImageIcon("king.png"),new ImageIcon("ace.png"),
+			 new ImageIcon("blueDeck.png"), new ImageIcon("BrownDeck.png"),
+			  new ImageIcon("Fetty.png")};
 	
 	
 	
@@ -24,23 +26,33 @@ public class board extends JFrame implements ActionListener{
 	private int player = 0;
 	private int cpui = 26;
 	private int playeri = 26;
-	private int king = 13;
-	private int queen = 12;
-	private int jack = 11;
+	/*private int king = 12;
+	private int queen = 11;
+	private int jack = 10;
 	private int ace = 13;
-	private int c2 = 2;
-	private int c3 = 3;
-	private int c4 = 4;
-	private int c5 = 5;
-	private int c6 = 6;
-	private int c7 = 7;
-	private int c8 = 8;
-	private int c9 = 9;
-	private int c10 = 10;
+	private int c2 = 1;
+	private int c3 = 2;
+	private int c4 = 3;
+	private int c5 = 4;
+	private int c6 = 5;
+	private int c7 = 6;
+	private int c8 = 7;
+	private int c9 = 8;
+	private int c10 = 9;*/
 	private int[] ci = new int[26];
 	private int[] pi = new int[26];
 	
+	public void inventory(){
+		for (int o=0; o<ci.length; o++){			
+				for (int i =0; i<ci.length / 2; i++){
+			ci[i] = 2+i;
+			pi[i] = 2+i;
 			
+		}
+		}
+	}
+	
+	
 	public board(){
 		this.setSize(200, 200);
 		this.setVisible(true);
@@ -48,8 +60,6 @@ public class board extends JFrame implements ActionListener{
 		this.setLayout(new GridLayout(3, 4));
 		
 		buildBoard();
-		
-		
 		
 	}
 			
@@ -60,8 +70,7 @@ public class board extends JFrame implements ActionListener{
 
 	public void buildBoard() {
 
-		
-		
+
 		for (int i = 0; i < theButtons.length; i++) {
 			theButtons[i] = new JButton("");
 			theButtons[i].setBackground(Color.WHITE);
@@ -84,15 +93,19 @@ public class board extends JFrame implements ActionListener{
 			this.cpu = (int)((Math.random()*13) + 1);
 			this.player = (int)((Math.random()*13) + 1);
 		System.out.println(player + " " + cpu);
+		theButtons[5].setIcon(picts[player-1]);
+		theButtons[6].setIcon(picts[cpu-1]);
 		
 	}
 	public void round(){
 		
-			
+		
 		if(player > cpu){
 			
 			this.playeri+=1;
 			this.cpui-=1;
+			yeahBaby();
+			
 			update();
 			//theButtons[7].setText("");
 			System.out.println("you win");
@@ -129,6 +142,17 @@ public class board extends JFrame implements ActionListener{
 			}
 		}
 	
+	public void yeahBaby(){
+		    try {
+		        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("H:/images/YeahBaby.wav").getAbsoluteFile());
+		        Clip clip = AudioSystem.getClip();
+		        clip.open(audioInputStream);
+		        clip.start();
+		    } catch(Exception ex) {
+		        System.out.println("Error with playing sound.");
+		        ex.printStackTrace();
+		    }
+		}
 	
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
